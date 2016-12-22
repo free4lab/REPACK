c8161150ce79fc511b0a2857e9cbb0ba3d4306fe
crystal6918 <crystal6918@126.com>
2016-12-22 16:14:37 +0800

    modify sass

--- src/dev/js/common/newappstore.js
+++ src/dev/js/common/newappstore.js
@@ -6,10 +6,17 @@ $(function(){
 		$(".categoryMenu"+$(this).data("menu"))
 		.removeClass("hidden")
 		.siblings().addClass("hidden");
+		
+		if(document.documentElement.clientWidth<768){
+			$(".as-carousel").css("marginTop","-247px")
+		}
 	});
 	$(".category").mouseleave(function(){
 		if(!$(".menus").hasClass("hidden")){
 			$(".menus").addClass("hidden");
 		}
+		$(".as-carousel").css("marginTop","0")
 	});
+	
+	
 });
